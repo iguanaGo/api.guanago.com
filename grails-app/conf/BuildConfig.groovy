984608facf2120f9c44bd38b7a015e118e4e6a35
paulofer85 <paulofer85@gmail.com>
2015-04-28 11:05:27 -0300

    + Modelos, Vistas y Controladores: Vuelo, Segmento y Escala + src.groovy: QPXServiceClient permite hacer request al GDS de QPX + src.groovy.Utils: FileUtils graban y levantan un archivo + searchFlight: boton en el controller de vuelo para consultar el GDS de QPX.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -51,6 +51,12 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.29'
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
+		
+		  runtime('org.codehaus.groovy.modules.http-builder:http-builder:0.5.1') {
+		        excludes 'xalan'
+		        excludes 'xml-apis'
+		        excludes 'groovy'
+		    }
     }
 
     plugins {
