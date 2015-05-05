2decfe1b9eb11b6567ddf2ccae1657e771e4cbef
Apivetta <aguspivetta@gmail.com>
2015-05-05 14:00:46 -0300

    plugin de rest api

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -67,6 +67,7 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
         compile ":asset-pipeline:2.1.5"
+        compile ":restful-api:1.0.0"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.8.1" // or ":hibernate:3.6.10.18"
