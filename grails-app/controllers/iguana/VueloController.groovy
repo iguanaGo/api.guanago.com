71d637c0e901bed8f0c55fccc335612befe5b203
paulofer85 <paulofer85@gmail.com>
2015-05-12 18:19:53 -0300

    ¬ ajuste

--- grails-app/controllers/iguana/VueloController.groovy
+++ grails-app/controllers/iguana/VueloController.groovy
@@ -139,7 +139,7 @@ class VueloController {
 			def slurper = new JsonSlurper()
 
 			//def result = new JsonBuilder( qpx.request(origen, destino, fecha, grailsApplication.config["QPX_KEY"] , grailsApplication.config["QPX_NUMBER_OF_RESULTS"].toInteger() ) )
-			def result = new JsonBuilder( qpx.request(origen, destino, fecha, "" , "" ) )
+			def result = new JsonBuilder( qpx.request(origen, destino, fecha, "" , 0 ) )
 			def flights = slurper.parseText('{ "flights":' + result + '}')
 
 			FileUtils.save(result,"result.json")
